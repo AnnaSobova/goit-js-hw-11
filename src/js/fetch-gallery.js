41c55b7e591dd2ec24f6ded68e31b44713711f89
Anna Sobova <anna_sobova@ukr.net>
2022-10-01 12:44:18 +0300

    add button in html

--- src/js/fetch-gallery.js
+++ src/js/fetch-gallery.js
@@ -1,27 +1,41 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
 const API_KEY = '30281232-70811b4bf68f3f563b5d60fb4';
+const BASE_URL = 'https://pixabay.com/api';
 
-export default class PixabayImages {
-    constructor() {
-      this.searchQuery = '';
-      this.page = 1;
-    }
-    get query() {
-      return this.searchQuery;
-    }
-    set query(newQuery) {
-      this.searchQuery = newQuery;
-    }
-    incrementPage() {
-      this.page += 1;
-    }
-    resetPage() {
-      this.page = 1;
-    }
-    getGalleryImages(name) {
-      return axios.get(
-        `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-      );
-    }
-  }
+export default class ImagesApiService { 
+constructor () {
+this.searchQ = "";
+this.page = 1;
+this.PER_PAGE = 40;
+}
+fetchImages() {
+ console.log ("до запроса", this);
+ const url = `${BASE_URL}/?key=${API_KEY}&
+ q=${this.searchQ}&page=${this.page}&per-page=${this.PER_PAGE}
+ &image_type=photo`;
+
+  return fetch(url)
+   .then(response => response.json())
+   .then(({ hits }) => {
+     console.log(hits)
+   this.incrementPage();
+    console.log("если ок", this)
+    return hits;
+   });
+};
+
+incrementPage() {
+ this.page += 1;
+}
+ 
+resetPage() {
+ this.page = 1;
+}
+
+get searchQuery() {
+return this.searchQ;
+}
+
+set searchQuery(newQuery){
+ this.searchQ = newQuery;
+ }
+}
